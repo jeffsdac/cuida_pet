d76b6c8564b9c60b0daa285324b1219820c1d381
Jefferson De Andrade <jeffersonandrade1999@gmail.com>
2025-11-04 21:25:00 -0300

    feat: model pet

---
+++ src/main/java/br/com/cuidapet/cuidapet/models/Pet/PetModel.java
@@ -0,0 +1,44 @@
+package br.com.cuidapet.cuidapet.models.Pet;
+
+import br.com.cuidapet.cuidapet.models.BaseModelClass;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "T_CUIDAPET_PET")
+@NoArgsConstructor
+@AllArgsConstructor
+public class PetModel extends BaseModelClass {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 30)
+    private String primeiroNome;
+
+    @Column(length = 30)
+    private String ultimoNome;
+
+    private Instant dataDeNascimento;
+
+    @Column(length = 500)
+    private String observacoes;
+
+    @Column(nullable = false, length = 25)
+    private String tipo;
+    // TODO: ENUM??
+
+    // private Porte porte;
+    // TODO: Enum
+
+    // private Temperamento temperamento;
+    // Todo: ENUM
+
+    // private Images fotoDoPet;
+    // TODO: incrementar com S3.
+}
