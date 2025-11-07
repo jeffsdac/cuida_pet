e8a1b51b3c4b2f8046101addd5b686b48002cab6
Jefferson De Andrade <jeffersonandrade1999@gmail.com>
2025-11-07 13:28:48 -0300

    feat: baseModelClass autoUpdate

--- src/main/java/br/com/cuidapet/cuidapet/CuidapetApplication.java
+++ src/main/java/br/com/cuidapet/cuidapet/CuidapetApplication.java
@@ -2,8 +2,10 @@ package br.com.cuidapet.cuidapet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CuidapetApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/cuidapet/cuidapet/models/BaseModelClass.java
+++ src/main/java/br/com/cuidapet/cuidapet/models/BaseModelClass.java
@@ -2,26 +2,30 @@ package br.com.cuidapet.cuidapet.models;
 
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 
 @MappedSuperclass
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class BaseModelClass {
 
     @CreatedDate
     @Column(nullable = false, updatable = false)
-    private Instant Created_at;
+    private Instant createdAt;
 
-    @LastModifiedBy
+    @LastModifiedDate
     @Column(nullable = false)
-    private Instant Updated_at;
+    private Instant updatedAt;
 
 }
